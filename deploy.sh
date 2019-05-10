77aaf67f3c5d6cb2f0afee894b37d3500fee7f11
yujichino <chino@jsl.co.jp>
2019-05-10 11:18:40 +0900

    mod sam path

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -e
 # *** deploy cloudformation yaml
-sam build && sam package --output-template-file packaged.yaml --s3-bucket jsl-aws-sam-test --profile smart_lock
-sam package --output-template-file packaged.yaml --s3-bucket jsl-aws-sam-test --profile smart_lock
+/usr/local/bin/sam build && sam package --output-template-file packaged.yaml --s3-bucket jsl-aws-sam-test --profile smart_lock
+/usr/local/bin/sam package --output-template-file packaged.yaml --s3-bucket jsl-aws-sam-test --profile smart_lock
