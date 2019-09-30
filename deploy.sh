020585eed4f11db828c61031684c2d9aa5f7a796
yujichino <chino@jsl.co.jp>
2019-09-30 16:05:03 +0900

    deploy test

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # *** deploy cloudformation yaml
 sam build && sam package --output-template-file packaged.yaml --s3-bucket jsl-aws-sam-test
-sam package --output-template-file packaged.yaml --s3-bucket jsl-aws-sam-test
+# sam package --output-template-file packaged.yaml --s3-bucket jsl-aws-sam-test
