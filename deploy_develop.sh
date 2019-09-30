37c2355faeed417b14a143c4fe066333163a38d7
yujichino <chino@jsl.co.jp>
2019-09-30 15:49:20 +0900

    deploy test

--- deploy_develop.sh
+++ deploy_develop.sh
@@ -2,3 +2,4 @@
 # *** deploy cloudformation yaml
 sam build && sam package --output-template-file packaged.yaml --s3-bucket jsl-aws-sam-staging
 sam package --output-template-file packaged.yaml --s3-bucket jsl-aws-sam-staging
+sam deploy --template-file packaged.yaml --stack-name sam-app-stg --capabilities CAPABILITY_IAM --region ap-northeast-1 --parameter-overrides Env=stg
