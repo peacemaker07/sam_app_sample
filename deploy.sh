89f8b6d3e7eef000501dc89c3b085172ad398117
yujichino <chino@jsl.co.jp>
2020-02-10 18:29:42 +0900

    mod deloy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
-# *** deploy cloudformation yaml
-sam build && sam package --output-template-file packaged.yaml --s3-bucket jsl-aws-sam-test
-sam package --output-template-file packaged.yaml --s3-bucket jsl-aws-sam-test
+
+sam build --use-container
+
+sam package \
+    --output-template-file packaged.yaml \
+    --s3-bucket jsl-aws-sam-test
+
+sam deploy --template-file packaged.yaml \
+    --stack-name sam-app-sample \
+    --capabilities CAPABILITY_NAMED_IAM CAPABILITY_IAM
